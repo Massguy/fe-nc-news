e354e1d51d5e3dc9d2b856669511a311ebf35446
OEM Configuration (temporary user) <stefin008@hotmail.co.uk>
2019-10-13 17:22:15 -0400

    sent username props to commentbyarticle

--- nc-news/src/App.js
+++ nc-news/src/App.js
@@ -10,46 +10,21 @@ import CommentByArticle from './Components/comments/CommentByArticle'
 import Error404 from './Components/Error404';
 class App extends Component {
   state = {
-    username: null,
-    loggedin: false
+    username: 'grumpy19'
   }
   render() {
+    const { username } = this.state
     return (
       <div className="App">
         <Header />
-        <Nav />
-        {!this.state.loggedin ? <label>
-          username  <input
-            onChange={this.usernameChange}
-            type='text'
-          />
-          <button
-            type='submit'
-            value='logIn'
-            onClick={this.LoggedIn}
-          >
-            Login
-              </button>
-        </label> : <span id='loggedIn'>
-            <p id='loggedInUser'>{`logged in as ${this.state.username}`}</p>
-
-            <button
-              onClick={this.LoggedOut}
-              className='button'
-              id='logOutButton'
-            >
-              Log Out
-              </button>
-            {this.state.loggedIn}
-          </span>
-        }
+        <Nav username={username} />
         <Router>
           <ArticleList path='/' />
           <ArticleList path='/articles' />
           <ArticleList path='/topics/:slug' />
           <SingleArticle path='/articles/:id' />
           <GetTopics path='/topics' />
-          <CommentByArticle path='/articles/:id/comments' />
+          <CommentByArticle path='/articles/:id/comments' username={username} />
           <Error404 default />
         </Router>
       </div>
