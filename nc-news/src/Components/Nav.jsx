1f7a54069f608712145c687aa1f491262bfbce7c
OEM Configuration (temporary user) <stefin008@hotmail.co.uk>
2019-10-13 17:25:25 -0400

    added login message

--- nc-news/src/Components/Nav.jsx
+++ nc-news/src/Components/Nav.jsx
@@ -4,12 +4,14 @@ import { Link } from "@reach/router";
 
 class Nav extends Component {
   render() {
+    const { username } = this.props
     return (
       <nav>
         <Link to='/'> <button>Home</button></Link>
         <Link to='/topics/cooking'><button>Cooking</button></Link>
         <Link to='/topics/football'><button>Football</button></Link>
         <Link to='/topics/coding'><button>Coding</button></Link>
+        <p>logged in as {username} </p>
       </nav>
     );;
   }
